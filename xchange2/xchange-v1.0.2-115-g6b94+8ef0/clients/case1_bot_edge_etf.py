dbe5cbfaedd995ac2abcd92f3135e5ffa2cbe50a
aaravbajaj012 <97072650+aaravbajaj012@users.noreply.github.com>
2023-04-14 04:38:28 -0400

    add etf arbitrage

--- xchange2/xchange-v1.0.2-115-g6b94+8ef0/clients/case1_bot_edge_etf.py
+++ xchange2/xchange-v1.0.2-115-g6b94+8ef0/clients/case1_bot_edge_etf.py
@@ -49,7 +49,7 @@ class Case1Bot(UTCBot):
             return pb.SwapResponse(False, "Unsure of swap")
         return await self.swap("redeem_etf_" + self.etf_suffix, qty) 
     
-    async def days_to_expiry(self, asset):
+    def days_to_expiry(self, asset):
         '''
         Calculates days to expiry for the future
         '''
@@ -65,9 +65,6 @@ class Case1Bot(UTCBot):
         #Competition event messages
         if kind == "generic_msg":
             msg = update.generic_msg.message
-            print(msg)
-            print(time.time())
-            print("_________________________")
             
             # Used for API DO NOT TOUCH
             if 'trade_etf' in msg:
@@ -101,7 +98,9 @@ class Case1Bot(UTCBot):
             for asset in CONTRACTS:
                 book = update.market_snapshot_msg.books[asset]
                 self._best_bid[asset] = float(book.bids[0].px)
-                self._best_ask[asset] = float(book.bids[0].px)
+                self._best_ask[asset] = float(book.asks[0].px)
+                self._best_bid_qty[asset] = int(book.bids[0].qty)
+                self._best_ask_qty[asset] = int(book.asks[0].qty)
             
 
 
@@ -112,14 +111,33 @@ class Case1Bot(UTCBot):
         self._best_bid: Dict[str, float] = defaultdict(
             lambda: 0
         )
+
+        self._best_bid_qty: Dict[str, int] = defaultdict(
+            lambda: 0
+        )
+
         ### Best Ask in the order book
         self._best_ask: Dict[str, float] = defaultdict(
             lambda: 0
         )
+
+        self._best_ask_qty: Dict[str, int] = defaultdict(
+            lambda: 0
+        )
+
         ### Order book for market making
         self.__orders: DefaultDict[str, Tuple[str, float]] = defaultdict(
             lambda: ("", 0)
         )
+
+        self._bid_orders: DefaultDict[str, Tuple[str, float]] = defaultdict(
+            lambda: ("", 0)
+        )
+
+        self._ask_orders: DefaultDict[str, Tuple[str, float]] = defaultdict(
+            lambda: ("", 0)
+        )
+
         ### TODO Recording fair price for each asset
         self._fair_price: DefaultDict[str, float] = defaultdict(
             lambda: 55
@@ -183,8 +201,76 @@ class Case1Bot(UTCBot):
         else:
             return self._best_ask[asset] - self._fair_price[asset] - 0.01
         
+    async def find_arbitrage(self):
+        # calculate bid price of ETF
+        # calculate fair value of underlying assets that compose ETF
+        # calculate the most recently expiring future
+        month = (self._day-1) // 21 + 1
+        contract1 = CONTRACTS[month + 1]
+        contract2 = CONTRACTS[month + 2]
+        contract3 = CONTRACTS[month + 3]
+        if self._best_ask["LLL"] < 5 * self._best_bid[contract1] + 3 * self._best_bid[contract2] + 2 * self._best_bid[contract3]:
+            minETFQty = min(self._best_ask_qty["LLL"], self._best_bid_qty[contract1], self._best_bid_qty[contract2], self._best_bid_qty[contract3])
+            r1 = await self.place_order(
+                        asset_code = "LLL",
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = minETFQty
+                    )
+            #self.__orders["LLL"] = (r1.order_id, r1.order_price)
+            r2 = await self.place_order(
+                        asset_code = contract1,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = minETFQty * 5
+                    )
+            r3 = await self.place_order(
+                        asset_code = contract2,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = minETFQty * 3
+                    )
+            r4 = await self.place_order(
+                        asset_code = contract3,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = minETFQty * 2
+                    )
+            
+            self.redeem_etf(minETFQty)
+        elif self._best_bid["LLL"] > 5 * self._best_ask[contract1] + 3 * self._best_ask[contract2] + 2 * self._best_ask[contract3]:
+            minETFQty = min(self._best_bid_qty["LLL"], self._best_ask_qty[contract1], self._best_ask_qty[contract2], self._best_ask_qty[contract3])
+            r1 = await self.place_order(
+                        asset_code = "LLL",
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = minETFQty
+                    )
+            #self.__orders["LLL"] = (r1.order_id, r1.order_price)
+            r2 = await self.place_order(
+                        asset_code = contract1,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = minETFQty * 5
+                    )
+            r3 = await self.place_order(
+                        asset_code = contract2,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = minETFQty * 3
+                    )
+            r4 = await self.place_order(
+                        asset_code = contract3,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = minETFQty * 2
+                    )
+            self.create_etf(minETFQty)
+            
+
+        
     async def make_market_asset(self, asset: str):
-        while self._day <= DAYS_IN_YEAR:
+        while self.days_to_expiry(asset) > 0:
             ## Old prices
             ub_oid, ub_price = self.__orders["underlying_bid_{}".format(asset)]
             ua_oid, ua_price = self.__orders["underlying_ask_{}".format(asset)]
