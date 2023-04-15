589aff078fda58ac2ce14491127fac61c01cc87d
aaravbajaj012 <97072650+aaravbajaj012@users.noreply.github.com>
2023-04-15 02:49:43 -0500

    levels and fade market making

--- Case 1/Analysis.py
+++ Case 1/Analysis.py
@@ -1,62 +1,102 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import optimize
 
 # Importing the dataset
 
 # Read the futures_2020 and futures_2021 and futures_2022 data from the csv file
-futures_2020 = pd.read_csv('futures_2020.csv')
-futures_2021 = pd.read_csv('futures_2021.csv')
-futures_2022 = pd.read_csv('futures_2022.csv')
+futures_dfs = [pd.read_csv('futures_2020.csv'), pd.read_csv('futures_2021.csv'), pd.read_csv('futures_2022.csv')]
 
 # Read the weather_2020 and weather_2021 and weather_2022 data from the csv file
-weather_2020 = pd.read_csv('weather_2020.csv')
-weather_2021 = pd.read_csv('weather_2021.csv')
-weather_2022 = pd.read_csv('weather_2022.csv')
+weathers_dfs = [pd.read_csv('weather_2020.csv'), pd.read_csv('weather_2021.csv'), pd.read_csv('weather_2022.csv')]
 
-# for each column in futures_2022, plot the data
-# have a legend for each column
-# make the ylim scale from 45 to 60
-# make the title "Futures 2022"
+# for each of the weather dataframes, check what the maximum daily change in weather index is
+# print the maximum daily change in weather index for each year
 
-plt.figure(figsize=(10, 6))
-for column in futures_2022:
-    if column:
-        plt.plot(futures_2022[column], label=column)
-plt.legend()
-plt.ylim(48, 62)
-plt.title('Futures 2022')
-plt.show()  
+for year in range(3):
+    print(weathers_dfs[year]['weather'].diff().max())
 
 
-# for each column in weather_2022, plot the data
-# have a legend for each column
-# make the ylim scale from 45 to 60
-# make the title "Weather 2022"
-# plot the expiry lines for each future
+# def obj_fn(shift, year):
+#     return futures_dfs[year]['SBL'].cov(weathers_dfs[year]['weather'].rolling(shift).mean())
 
-plt.figure(figsize=(10, 6))
-for column in weather_2022:
-    if column:
-        plt.plot(weather_2022[column], label=column)
-plt.axvline(x=21, color='r', linestyle='--')
-plt.axvline(x=42, color='r', linestyle='--')
-plt.axvline(x=63, color='r', linestyle='--')
-plt.axvline(x=84, color='r', linestyle='--')
-plt.axvline(x=105, color='r', linestyle='--')
-plt.axvline(x=126, color='r', linestyle='--')
-plt.axvline(x=147, color='r', linestyle='--')
-plt.axvline(x=168, color='r', linestyle='--')
-plt.axvline(x=189, color='r', linestyle='--')
-plt.axvline(x=210, color='r', linestyle='--')
-plt.axvline(x=231, color='r', linestyle='--')
-plt.axvline(x=252, color='r', linestyle='--')
-plt.legend()
-plt.ylim(-3, 3)
-plt.title('Weather 2022')
-plt.show()
 
-# look for a correlation between the futures and weather
-# try to fit a regression line to the data
-# plot the regression line on top of the data
+# shifts = []
 
+# for year in range(3):
+#     max_cov = None
+#     max_shift = None
+#     # Find the shift that maximizes the covariance by brute force
+#     for i in range(2, 50):
+#         if max_cov is None or obj_fn(i, year) > max_cov:
+#             max_cov = obj_fn(i, year)
+#             max_shift = i
+
+#     shifts.append(max_shift)
+
+# shifts = [20, 20, 20]
+
+# # apply a rolling mean of 5 days to the weather data
+# for year in range(3):
+#     weathers_dfs[year]['weather'] = weathers_dfs[year]['weather'].rolling(shifts[year]).mean()
+
+# # print the correlation between the futures and weather data
+# for year in range(3):
+#     print(futures_dfs[year]['SBL'].corr(weathers_dfs[year]['weather']))
+
+# print(shifts)
+
+# # plot the weather day and the SBL futures for each year on the same plot with two axes
+# # plot all three plots on one figure as subplots
+
+# # fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+
+# # for year in range(3):
+# #     axs2 = axs[year].twinx()
+    
+# #     axs[year].plot(futures_dfs[year]['SBL'], label='SBL', color='red')
+# #     axs[year].twinx().plot(weathers_dfs[year]['weather'], label='weather', color='blue')
+# #     axs[year].set_title(f'Year {year}')
+# #     axs[year].legend()
+
+
+# # plt.show()
+
+
+# # plot the weather on the x-axis and the SBL futures on the y-axis for each year on the same plot
+# # plot all three plots on one figure as subplots
+
+# # fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+
+# # for year in range(3):
+# #     axs[year].scatter(weathers_dfs[year]['weather'], futures_dfs[year]['SBL'])
+# #     axs[year].set_title(f'Year {year}')
+
+# # plt.show()
+
+# # # for each year, drop the NaN rows, and only keep the rows with indices that exist in both dataframes
+# for year in range(3):
+#     futures_dfs[year] = futures_dfs[year].dropna()
+#     weathers_dfs[year] = weathers_dfs[year].dropna()
+
+#     futures_dfs[year] = futures_dfs[year].loc[weathers_dfs[year].index]
+#     weathers_dfs[year] = weathers_dfs[year].loc[futures_dfs[year].index]
+
+# # # find the best fit line for each year and find the constants a and b for the equation y = ax + b
+# # # print the a and b for each year
+
+# mss = []
+# bss = []
+
+# for year in range(3):
+#     m, b = np.polyfit(weathers_dfs[year]['weather'], futures_dfs[year]['SBL'], 1)
+
+#     mss.append(m)
+#     bss.append(b)
+
+# m_slope, m_yint = np.polyfit([0, 1, 2], mss, 1)
+# b_slope, b_yint = np.polyfit([0, 1, 2], bss, 1)
+
+# for i in range(3, 7):
+#     print("202" + str(i) + ": m =", m_slope * i + m_yint, " b =", b_slope * i + b_yint)
--- xchange2/xchange-v1.0.2-115-g6b94+8ef0/clients/case1_bot_edge_etf.py
+++ xchange2/xchange-v1.0.2-115-g6b94+8ef0/clients/case1_bot_edge_etf.py
@@ -8,6 +8,11 @@ import proto.utc_bot as pb
 import betterproto
 import asyncio
 import re
+import time
+import json
+import sys
+
+# Constants
 
 DAYS_IN_MONTH = 21
 DAYS_IN_YEAR = 252
@@ -16,27 +21,28 @@ NUM_FUTURES = 14
 TICK_SIZE = 0.01
 CARRYING_COST = 0.10
 FUTURE_CODES = [chr(ord('A') + i) for i in range(NUM_FUTURES)] # Suffix of monthly future code
-CONTRACTS = ['SBL'] +  ['LBS' + c for c in FUTURE_CODES] + ['LLL']
+FUTURES = ['LBS' + c for c in FUTURE_CODES]
+CONTRACTS = ['SBL'] +  FUTURES + ['LLL']
+
+EPSILON = 0.0001
+
 
 MOVING_AVG_WINDOW = 20
-
 MIN_EDGE = 0.10
-FADE = 0.01
+FADE = 0.20
 SLACK = 0.10
 NUM_LEVELS = 4
-
-LEVEL_SPREAD = 0.02
-
+LEVEL_SPREAD = 1.10
 ORDER_SIZE = 60
-
 LEVELS_ENABLED = True
 
+WEATHER_TO_PRICE_M = 1.7838804404379633
+WEATHER_TO_PRICE_B = 54.849618905657714
 # 2023: m = 1.7838804404379633  b = 54.849618905657714
 # 2024: m = 1.60005651307329  b = 54.890769908266975
 # 2025: m = 1.4162325857086162  b = 54.931920910876244
 # 2026: m = 1.2324086583439426  b = 54.973071913485505
-WEATHER_TO_PRICE_M = 1.7838804404379633
-WEATHER_TO_PRICE_B = 54.849618905657714
+
 
 # TUNE Weights
 WEIGHTS = {"WEATHER_FAIR": 0.3, "SOYBEAN_MARKET" : 0.4, "FUTURES_MARKET" : 0.3}
@@ -80,10 +86,11 @@ class Case1Bot(UTCBot):
 
         kind, _ = betterproto.which_one_of(update, "msg")
 
+        msg = None
+
         #Competition event messages
         if kind == "generic_msg":
             msg = update.generic_msg.message
-            print(type(msg))
             
             # Used for API DO NOT TOUCH
             if 'trade_etf' in msg:
@@ -94,27 +101,23 @@ class Case1Bot(UTCBot):
                 weather = float(re.findall("\d+\.\d+", msg)[0])
                 self._weather_log_PS.append(self._weather_log_PS[-1] + weather)
                 self._weather_log.append(weather)
-                for asset in CONTRACTS:
+
+                for asset in FUTURES:
                     self._fair_price[asset] = self.calculate_future_fair_price_weighted(asset, WEIGHTS)
-                
+
             # Updates date
             if "Day" in msg:
                 self._day = int(re.findall("\d+", msg)[0])
-                            
-            # Updates positions if unknown message (probably etf swap)
-            else:
-                resp = await self.get_positions()
-                if resp.ok:
-                    self.positions = resp.positions
 
-         
+ 
         elif kind == "market_snapshot_msg":
             for asset in CONTRACTS:
                 book = update.market_snapshot_msg.books[asset]
-                self._best_bid[asset] = float(book.bids[0].px)
-                self._best_ask[asset] = float(book.asks[0].px)
-                self._best_bid_qty[asset] = int(book.bids[0].qty)
-                self._best_ask_qty[asset] = int(book.asks[0].qty)
+                if not (len(book.bids) == 0 or len(book.asks) == 0):
+                    self._best_bid[asset] = float(book.bids[0].px)
+                    self._best_ask[asset] = float(book.asks[0].px)
+                    self._best_bid_qty[asset] = int(book.bids[0].qty)
+                    self._best_ask_qty[asset] = int(book.asks[0].qty)
 
                 # arbitrage where future > soybeans + carrying cost * days to expiry (buy soybeans and short future)
                 #    best future bid > best soybean ask + CC * N AND both "bests" are not self trades => 
@@ -126,7 +129,17 @@ class Case1Bot(UTCBot):
                 #    best future ask < best soybean bid AND both "bests" are not self trades => 
                 #        bid at best future ask, ask at best soybeans bid
                 #        (quantity = min(best futures ask qty, best soybeans bid qty)) 
-                
+
+        elif kind == "fill_msg":
+            resp = await self.get_positions()
+            if resp.ok:
+                self.positions = resp.positions
+
+        elif kind == "order_cancelled_msg":
+            msg = update.order_cancelled_msg.message
+
+        elif kind == "request_failed_msg":
+            msg = update.request_failed_msg.message
 
 
     async def handle_round_started(self):
@@ -155,30 +168,24 @@ class Case1Bot(UTCBot):
             lambda: 0
         )
 
-        self._ask_order_px: Dict[str, float] = defaultdict(
-            lambda: 0
-        )
-
         self._bid_order_qty: Dict[str, int] = defaultdict(
             lambda: 0
         )
-        
-        self._ask_order_qty: Dict[str, int] = defaultdict(
-            lambda: 0
-        )
-        
+
         self.bid_id: Dict[str, str] = defaultdict(
             lambda: ""
         )
 
-        self._ask_id: Dict[str, str] = defaultdict(
-            lambda: ""
+        self._ask_order_px: Dict[str, float] = defaultdict(
+            lambda: 0
         )
 
-
-        ### TODO Recording fair price for each asset
-        self._fair_price: DefaultDict[str, float] = defaultdict(
-            lambda: 55
+        self._ask_order_qty: Dict[str, int] = defaultdict(
+            lambda: 0
+        )
+        
+        self._ask_id: Dict[str, str] = defaultdict(
+            lambda: ""
         )
 
         self._bid_levels: DefaultDict[str, List[str]] = defaultdict(
@@ -189,6 +196,10 @@ class Case1Bot(UTCBot):
             lambda: [str(i+NUM_LEVELS) for i in range(NUM_LEVELS)]
         )
 
+        self._fair_price: DefaultDict[str, float] = defaultdict(
+            lambda: 55
+        )
+
         
         ### List of weather reports
         self._weather_log = []
@@ -197,9 +208,9 @@ class Case1Bot(UTCBot):
         
         await asyncio.sleep(.1)
         
-        # Starts market making for each asset
-        # for asset in CONTRACTS:
-        #     asyncio.create_task(self.make_market_asset(asset))
+        # Starts market making for each future
+        for asset in FUTURES:
+            asyncio.create_task(self.make_market_asset(asset))
 
 
     def calculate_soybean_fair_price(self):
@@ -235,35 +246,39 @@ class Case1Bot(UTCBot):
         return weights["WEATHER_FAIR"] * future_weather_fp + weights["SOYBEAN_MARKET"] * future_SB_market_fp + weights["FUTURES_MARKET"] * future_market_fp
 
     def calculate_bid_edge(self, asset):
-        if self._fair_price[asset] - self._best_bid[asset] < MIN_EDGE:
-            return MIN_EDGE
-        elif self._fair_price[asset] - self._best_bid[asset] > (MIN_EDGE + SLACK):
+        isOurs = (self._best_bid[asset] - self._bid_order_px[asset]) < EPSILON
+        diff = self._fair_price[asset] - self._best_bid[asset]
+        if diff < MIN_EDGE:
+            if isOurs:
+                return diff
+            else:
+                return MIN_EDGE
+        elif diff > (MIN_EDGE + SLACK):
             return MIN_EDGE + SLACK
         else:
-            return self._fair_price[asset] - self._best_bid[asset] - 0.01
+            if isOurs:
+                return diff
+            else:
+                return diff - 0.01
         
     def calculate_ask_edge(self, asset):
-        if self._best_ask[asset] - self._fair_price[asset] < MIN_EDGE:
-            return MIN_EDGE
-        elif self._best_ask[asset] - self._fair_price[asset] > (MIN_EDGE + SLACK):
+        isOurs = (self._ask_order_px[asset] - self._best_ask[asset]) < EPSILON
+        diff = self._best_ask[asset] - self._fair_price[asset]
+        if diff < MIN_EDGE:
+            if isOurs:
+                return diff
+            else:
+                return MIN_EDGE
+        elif diff > (MIN_EDGE + SLACK):
             return MIN_EDGE + SLACK
         else:
-            return self._best_ask[asset] - self._fair_price[asset] - 0.01
+            if isOurs:
+                return diff
+            else:
+                return diff - 0.01
 
-    async def place_levels(self, asset: str, bid_px: float, ask_px : float):
-        for i in range(1, NUM_LEVELS+1):
-
-            r = await self.modify_order(
-                order_id=self._bid_levels[asset][i-1],
-                asset_code = asset,
-                order_type = pb.OrderSpecType.LIMIT,
-                order_side = pb.OrderSpecSide.BID,
-                qty = ORDER_SIZE,
-                price = round_nearest(bid_px - i * LEVEL_SPREAD)
-            )
-
-            self._bid_levels[asset][i-1] = r.order_id
-
+    async def place_ask_levels(self, asset: str, ask_px : float):
+        for i in range(1, NUM_LEVELS + 1):
             r = await self.modify_order(
                 order_id=self._ask_levels[asset][i-1],
                 asset_code = asset,
@@ -275,34 +290,201 @@ class Case1Bot(UTCBot):
 
             self._ask_levels[asset][i-1] = r.order_id
 
+    async def place_bid_levels(self, asset: str, bid_px: float):
+        for i in range(1, NUM_LEVELS + 1):
+            r = await self.modify_order(
+                order_id=self._bid_levels[asset][i-1],
+                asset_code = asset,
+                order_type = pb.OrderSpecType.LIMIT,
+                order_side = pb.OrderSpecSide.BID,
+                qty = ORDER_SIZE,
+                price = round_nearest(bid_px - i * LEVEL_SPREAD)
+            )
+
+            self._bid_levels[asset][i-1] = r.order_id
+
+
+    def fade(self, asset):
+        return FADE * self.positions[asset]
         
     async def make_market_asset(self, asset: str):
 
         while self.days_to_expiry(asset) >= 0:
-            await asyncio.sleep(.1)
-            # if days_to_expiry > 1:
-                # get the fair price
-                # get the current position size
-                # get the faded fair price
-                # calculate edge around faded fair price using edge/slack/penny in
-                # place order pair and levels (make sure to do it in right order)
-                
-                # update the relevant variables 
-                # - update current market bid ask prices
-                # - update new levels
-        
-            # elif days_to_expiry == 1:
-            #  - close our current open positions
-            #     - if you have a long position:
-            #         - (if best market future bid price > current market soybean bid price) (sell futures)
-            #         - (else sell soybean)
-            #     - else: (short positions)
-            #         - 
-            #  - cancel current orders for that asset
 
-            # else: (<= 0)
-            # - clear all soybean positions 
-            
+            if self.days_to_expiry(asset) > 1:
+                # get the fair price
+                fair_px =  self._fair_price[asset]
+
+                # get the faded fair price
+                faded_fair_px = fair_px - self.fade(asset)
+
+                # calculate edge around faded fair price using edge/slack/penny in
+                bid_edge = self.calculate_bid_edge(asset)
+                ask_edge = self.calculate_ask_edge(asset)
+
+                # place order pair and levels (make sure to do it in right order)
+                order_bid = round_nearest(faded_fair_px - bid_edge)
+                order_ask = round_nearest(faded_fair_px + ask_edge)
+
+                # if our new bid is higher than our current ask, 
+                #   then update all the ask levels
+                #   then update the ask order
+                #   then update the bid order
+                #   then update the bid levels
+                if order_bid > self._best_ask[asset]:
+                    await self.place_ask_levels(asset, order_ask)
+
+                    r = await self.modify_order(
+                        order_id=self.ask_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = ORDER_SIZE,
+                        price = order_ask
+                    )
+                    self.ask_id[asset] = r.order_id
+                    
+                    r = await self.modify_order(
+                        order_id=self.bid_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = ORDER_SIZE,
+                        price = order_bid
+                    )
+                    self.bid_id[asset] = r.order_id
+
+                    await self.place_bid_levels(asset, order_bid)
+
+                # if our new ask is lower than our current bid,
+                #   then update all the bid levels
+                #   then update the bid order
+                #   then update the ask order
+                #   then update the ask levels
+                elif order_ask < self._best_bid[asset]:
+                    await self.place_bid_levels(asset, order_bid)
+
+                    r = await self.modify_order(
+                        order_id=self.bid_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = ORDER_SIZE,
+                        price = order_bid
+                    )
+                    self.bid_id[asset] = r.order_id
+
+                    r = await self.modify_order(
+                        order_id=self.ask_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = ORDER_SIZE,
+                        price = order_ask
+                    )
+                    self.ask_id[asset] = r.order_id
+
+                    await self.place_ask_levels(asset, order_ask)
+                else:
+                    # if we are here, then we are in the middle of the spread
+                    #   so we need to update the bid and ask orders
+                    #   then update the bid and ask levels
+                    r = await self.modify_order(
+                        order_id=self.bid_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = ORDER_SIZE,
+                        price = order_bid
+                    )
+                    self.bid_id[asset] = r.order_id
+
+                    r = await self.modify_order(
+                        order_id=self.ask_id[asset],
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.LIMIT,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = ORDER_SIZE,
+                        price = order_ask
+                    )
+                    self.ask_id[asset] = r.order_id
+
+                    await self.place_bid_levels(asset, order_bid)
+                    await self.place_ask_levels(asset, order_ask)
+
+                
+                self._best_bid[asset] = order_bid
+                self._best_ask[asset] = order_ask
+                
+        
+            elif self.days_to_expiry(asset) == 1:
+                # cancel all orders
+                #  - close our current open positions
+                #     - if you have a long position:
+                #         - (if best market future bid price > current market soybean bid price) (sell futures)
+                #         - (else sell soybean)
+                #     - else: (short positions)
+                #         -  (if best market future ask price < current market soybean ask price) (buy futures)
+                #         - (else buy soybean)
+
+
+                # cancel all orders
+                r = await self.cancel_orders(self.bid_id[asset])
+                r = await self.cancel_orders(self.ask_id[asset])
+                for i in range(1, NUM_LEVELS+1):
+                    r = await self.cancel_orders(self._bid_levels[asset][i-1])
+                    r = await self.cancel_orders(self._ask_levels[asset][i-1])
+
+                # long on future, so sell
+                if self.positions[asset] > 0:
+                    if self._best_bid[asset] > self._best_bid['SBL']:
+                        r = await self.place_order(
+                            asset_code = asset,
+                            order_type = pb.OrderSpecType.MARKET,
+                            order_side = pb.OrderSpecSide.ASK,
+                            qty = self.positions[asset]
+                        )
+                    else:
+                        r = await self.place_order(
+                            asset_code = 'SBL',
+                            order_type = pb.OrderSpecType.MARKET,
+                            order_side = pb.OrderSpecSide.ASK,
+                            qty = self.positions[asset]
+                        )
+
+                # short on future, so buy
+                elif self.positions[asset] < 0:
+                    if self._best_ask[asset] < self._best_ask['SBL']:
+                        r = await self.place_order(
+                            asset_code = asset,
+                            order_type = pb.OrderSpecType.MARKET,
+                            order_side = pb.OrderSpecSide.BID,
+                            qty = abs(self.positions[asset])
+                        )
+                    else:
+                        r = await self.place_order(
+                            asset_code = 'SBL',
+                            order_type = pb.OrderSpecType.MARKET,
+                            order_side = pb.OrderSpecSide.BID,
+                            qty = abs(self.positions[asset])
+                        )
+
+            else:
+                # - clear all soybean positions 
+                if self.positions[asset] > 0:
+                    r = await self.place_order(
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.ASK,
+                        qty = self.positions[asset]
+                    )
+                elif self.positions[asset] < 0:
+                    r = await self.place_order(
+                        asset_code = asset,
+                        order_type = pb.OrderSpecType.MARKET,
+                        order_side = pb.OrderSpecSide.BID,
+                        qty = abs(self.positions[asset])
+                    )
        
         
 
@@ -311,4 +493,42 @@ def round_nearest(x):
 
 
 if __name__ == "__main__":
+    # read the first argument from the command line
+    #   - this is the name of the config file
+    #   - if no argument is given, then do nothing
+
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+        with open(config_file) as f:
+            config = json.load(f)
+
+            # using the config, update all of the following variables
+                # MOVING_AVG_WINDOW = 20
+                # MIN_EDGE = 0.10
+                # FADE = 0.20
+                # SLACK = 0.10
+                # NUM_LEVELS = 4
+                # LEVEL_SPREAD = 1.10
+                # ORDER_SIZE = 60
+                # LEVELS_ENABLED = True
+
+                # WEATHER_TO_PRICE_M = 1.7838804404379633
+                # WEATHER_TO_PRICE_B = 54.849618905657714
+
+            MOVING_AVG_WINDOW = config['moving_avg_window']
+            MIN_EDGE = config['min_edge']
+            FADE = config['fade']
+            SLACK = config['slack']
+            NUM_LEVELS = config['num_levels']
+            LEVEL_SPREAD = config['level_spread']
+            ORDER_SIZE = config['order_size']
+            LEVELS_ENABLED = config['levels_enabled']
+
+            WEATHER_TO_PRICE_M = config['weather_to_price_m']
+            WEATHER_TO_PRICE_B = config['weather_to_price_b']
+
+            bot = Case1Bot()
+            
+
+
     start_bot(Case1Bot)
